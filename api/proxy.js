b02d8f1f19271ac715bf01866787e6d94b59c7ab
xfy196 <xfy196@outlook.com>
2021-04-05 12:27:51 +0800

    init api proxy

---
+++ api/proxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  if (req.url.startsWith('/api')) {
+    target = 'http://47.98.159.95/m-api/'
+  }
+
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/': '/m-api/'
+    }
+  })(req, res)
+}
