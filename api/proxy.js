697bc3a14383efe7e19fa32785afc94fed7d272c
xfy196 <xfy196@outlook.com>
2021-04-05 13:27:07 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'http://47.98.159.95/m-api/'
+    target = 'http://47.98.159.95'
   }
 
   // 创建代理对象并转发请求
