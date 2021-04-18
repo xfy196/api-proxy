38cc78ebc8a42f3e02ebd3be592dc6446ea6cdf3
xfy196 <xfy196@outlook.com>
2021-04-18 14:46:35 +0800

    更新最新地址

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'https://api.mtnhao.com'
+    target = 'http://47.98.159.95'
   }
 
   // 创建代理对象并转发请求
@@ -14,7 +14,7 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/api/': '/'
+      '^/api/': '/m-api/'
     }
   })(req, res)
 }
