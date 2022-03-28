6c430cd5132ef35c4fd904f786312fc0d05fb093
许富荧 <xufuying@xufuyingdeMacBook-Pro.local>
2022-03-28 16:48:23 +0800

    更新tinify图片压缩接口

--- api/proxy.js
+++ api/proxy.js
@@ -2,15 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target = ''
-  let base = ''
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    base = '/api'
-    target = 'https://api.tinify.com'
-  }else if(req.url.startsWith('/tinifyapi')){
-    base = '/tinifyapi'
-    target = "https://api.tinify.com"
+    target = 'https://api.music.xxytime.top'
   }
 
   // 创建代理对象并转发请求
@@ -18,7 +13,7 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      [`^${base}/`]: '/'
+      '^/api/': '/'
     }
   })(req, res)
 }
