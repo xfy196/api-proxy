e4b59e867439c9fe52df0c65cb33445b2d553cde
许富荧 <xfy196@outlook.com>
2024-04-22 15:53:46 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'https://api.music.xxytime.top'
+    target = 'https://apimusic.xxytime.top'
   }
 
   // 创建代理对象并转发请求
