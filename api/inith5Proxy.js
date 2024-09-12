e71bccea1fed74affef9768ac79ce59404f82cce
xufuying <xufuying@100doc.com.cn>
2024-09-12 23:11:52 +0800

    update

--- api/inith5Proxy.js
+++ api/inith5Proxy.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  if (req.url.startsWith('/api/h5')) {
+  if (req.url.startsWith('/api')) {
     target = 'http://36.7.140.35:18080'
   }
   // 创建代理对象并转发请求
