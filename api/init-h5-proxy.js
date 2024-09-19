5ba3916b843a83f18cfe8b387be513977ffb37a9
许富荧 <xfy196@outlook.com>
2024-09-19 10:30:47 +0800

    update

--- api/init-h5-proxy.js
+++ api/init-h5-proxy.js
@@ -4,15 +4,15 @@ module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  if (req.url.startsWith('/h5')) {
+  if (req.url.startsWith('/api/h5')) {
     target = 'http://36.7.140.35:18080'
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    // pathRewrite: {
-    //   '^/api': '/'
-    // }
+    pathRewrite: {
+      '^/api': '/'
+    }
   })(req, res)
 }
